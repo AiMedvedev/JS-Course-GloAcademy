7deb97369a53c897b3ab14a6b5694fd44c18a585
AiMedvedev <forcablaugrana@gmail.com>
2022-03-24 17:42:17 +0300

    задания 1 урока сделаны

---
+++ script.js
@@ -0,0 +1,9 @@
+const title = 'New Project';
+const screens = [];
+let screenPrice = 1500,
+    rollback = 20,
+    fullPrice = 8000,
+    adaptive = true;
+
+console.log('Выполняю задание 1 урока');
+alert("Задание 1 урока выполнено");
