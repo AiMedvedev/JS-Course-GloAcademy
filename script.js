dc8256d42ab9bec76a48229e6fbd13552ed623f3
AiMedvedev <forcablaugrana@gmail.com>
2022-03-25 11:39:16 +0300

    Обязательное задание урока 2 сделано

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const title = 'Project 24';
-const screens = 'Простые, Ложные, Интеративные';
+const screens = 'Простые, Сложные, Интеративные';
 const rollback = 25;
 let screenPrice = 1500,
     fullPrice = 8000,
     adaptive = true;
 
-console.log('Выполняю задание 1 урока');
-alert("Задание 1 урока выполнено");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
+
+console.log(screens.toLowerCase().split(", "));
+
+console.log(`Процент отката посреднику за работу составляет ${fullPrice * (rollback/100)} рублей`);
