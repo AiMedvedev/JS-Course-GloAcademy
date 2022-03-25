464f6678a1132cd20734b58376b6eb3849567436
AiMedvedev <forcablaugrana@gmail.com>
2022-03-25 11:10:42 +0300

    задание 1.1 выполнено

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const title = 'New Project';
-const screens = [];
-const rollback = () => {};
+const title = 'Project 24';
+const screens = 'Простые, Ложные, Интеративные';
+const rollback = 25;
 let screenPrice = 1500,
     fullPrice = 8000,
     adaptive = true;
